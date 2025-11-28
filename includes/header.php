6ee16823ce6a874f1bc06a3049e6154ed6180fb9
Daniel Briones Vargas <danielbrionesvargas1@gmail.com>
2025-11-28 13:08:50 -0600

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <header class="border-b border-border sticky top-0 z-50 headerBG">
     <div class="container mx-auto px-4 py-4 flex items-center justify-between">
         <a href="/" class="text-2xl font-bold text-primary hover:text-primary/90 transition-colors headerText">
-            <img src="../assets/img/lakeland_commissary__1__720.png" alt="">
+            <img src="../assets/img/lakeland_commissary__1__720.png" style="width: 250px;" alt="">
         </a>
         <nav class="md:flex items-center gap-8">
             <a href="#home" class="hover:text-primary transition-colors headerText">
